f60ada9af6e95e5ab32530c812d3e93882d66eb3
aallnneess <73443724+aallnneess@users.noreply.github.com>
2024-07-25 09:43:39 +0200

    test2

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const os = require('os');
+
 router.get('/', (req,res) => {
    res.send('Hello World!');
 });
 
 router.get('/status', (req, res) => {
-    res.send('Hello World - Status!');
+    res.send({
+        freemem: os.freemem() / 1024 / 1024
+    });
 });
 
 module.exports = router;
