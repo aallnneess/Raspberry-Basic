3be8e5b2c43566e5ac62080f4fc98cb9092599ff
aallnneess <73443724+aallnneess@users.noreply.github.com>
2024-07-25 18:17:42 +0200

    webcam test2

--- routes/webcam.js
+++ routes/webcam.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { exec } = require('child_process');
+const path = require('path');
 
 router.get('/', (req, res) => {
     res.send('Welcome to the Webcam Stream');
+});
+
+const imagePath = path.join(__dirname, '..', 'public', 'images', 'capture.jpg');
+
+router.get('/capture', (req, res) => {
+    // Shell-Kommando zum Aufnehmen eines Bildes
+    exec(`raspistill -o ${imagePath}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Fehler beim Aufnehmen des Bildes: ${error.message}`);
+            return res.status(500).send('Fehler beim Aufnehmen des Bildes');
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return res.status(500).send('Fehler beim Aufnehmen des Bildes');
+        }
+
+        // Senden des aufgenommenen Bildes als Antwort
+        res.sendFile(imagePath);
+    });
 });
 
 module.exports = router;
