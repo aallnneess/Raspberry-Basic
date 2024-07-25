984aeda05eeadf67508544cc04647b2fc54f205c
aallnneess <73443724+aallnneess@users.noreply.github.com>
2024-07-25 10:51:48 +0200

    test19

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.get('/status', async (req, res) => {
 
     // CPU-Auslastung in Prozent
     const currentLoad = await si.currentLoad();
-    const cpuUsage = currentLoad.currentLoad;
+    const cpuUsage = currentLoad.avgLoad;
 
     // WLAN Signalstärke
     exec("iwconfig wlan0 | grep 'Link Quality'", (error, stdout, stderr) => {
