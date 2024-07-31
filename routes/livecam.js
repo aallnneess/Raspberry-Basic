dcbe304cab726af31c5d9b99054997f12f6e4bb3
aallnneess <73443724+aallnneess@users.noreply.github.com>
2024-07-31 11:02:20 +0200

    webcam test18

--- routes/livecam.js
+++ routes/livecam.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { spawn } = require('child_process');
 
-// router.get('/', (req, res) => {
-//     res.send('Hello from Video Stream');
-// });
-
 router.get('/', (req, res) => {
     res.send('<html><body><video src="/liveCam/stream" controls autoplay></video></body></html>');
 });
@@ -13,29 +9,30 @@ router.get('/', (req, res) => {
 router.get('/stream', (req, res) => {
     res.writeHead(200, {
         'Content-Type': 'video/mp4',
+        'Content-Disposition': 'inline',
         'Transfer-Encoding': 'chunked'
     });
 
-    console.log('Starting rpicam-vid...');
-    const rpicam = spawn('rpicam-vid', ['-t', '0', '-o', '-', '--inline']);
+    console.log('Starting ffmpeg...');
+    const ffmpeg = spawn('ffmpeg', ['-f', 'v4l2', '-i', '/dev/video0', '-c:v', 'libx264', '-f', 'mp4', '-movflags', 'frag_keyframe+empty_moov', '-']);
 
-    rpicam.stdout.on('data', (data) => {
+    ffmpeg.stdout.on('data', (data) => {
         console.log('Streaming data...');
         res.write(data);
     });
 
-    rpicam.stderr.on('data', (data) => {
+    ffmpeg.stderr.on('data', (data) => {
         console.error(`stderr: ${data}`);
     });
 
-    rpicam.on('close', (code) => {
+    ffmpeg.on('close', (code) => {
         console.log(`child process exited with code ${code}`);
         res.end();
     });
 
     req.on('close', () => {
-        console.log('Request closed, killing rpicam-vid...');
-        rpicam.kill();
+        console.log('Request closed, killing ffmpeg...');
+        ffmpeg.kill();
     });
 });
 
