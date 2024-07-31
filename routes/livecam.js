5b7b63a0a05593179cbe0289f111bddf52f59a48
aallnneess <73443724+aallnneess@users.noreply.github.com>
2024-07-31 10:46:13 +0200

    webcam test15

--- routes/livecam.js
+++ routes/livecam.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { spawn } = require('child_process');
+
+// router.get('/', (req, res) => {
+//     res.send('Hello from Video Stream');
+// });
 
 router.get('/', (req, res) => {
-    res.send('Hello from Video Stream');
+    res.send('<html><body><video src="/liveCam/stream" controls autoplay></video></body></html>');
+});
+
+router.get('/stream', (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'video/mp4',
+        'Transfer-Encoding': 'chunked'
+    });
+
+    const rpicam = spawn('rpicam-vid', ['-t', '0', '-o', '-', '--inline']);
+    rpicam.stdout.pipe(res);
+
+    req.on('close', () => {
+        rpicam.kill();
+    });
 });
 
 module.exports = router;
