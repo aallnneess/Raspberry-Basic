c487855a3d5d9eabc3e8610793ffeed20832d792
aallnneess <73443724+aallnneess@users.noreply.github.com>
2024-07-31 11:56:26 +0200

    webcam test21

--- routes/livecam.js
+++ routes/livecam.js
@@ -1,60 +1,9 @@
 const express = require('express');
-const { spawn } = require('child_process');
 const router = express.Router();
+const { spawn } = require('child_process');
 
-const app = express();
-const PORT = 3000;
-
-// Route für die Live-Stream-Seite
-router.get('/live', (req, res) => {
-    res.send(`
-        <html>
-        <body>
-            <h1>Live Stream</h1>
-            <video id="video" width="640" height="480" controls autoplay></video>
-            <script>
-                var video = document.getElementById('video');
-                var socket = new WebSocket('ws://' + window.location.hostname + ':3000/stream');
-                socket.binaryType = 'arraybuffer';
-                socket.onmessage = function(event) {
-                    var blob = new Blob([event.data], { type: 'video/mp4' });
-                    video.src = URL.createObjectURL(blob);
-                };
-            </script>
-        </body>
-        </html>
-    `);
-});
-
-// Route für den WebSocket-Stream
-const server = require('http').createServer(app);
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ server, path: '/stream' });
-
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    const rpicam = spawn('rpicam-vid', ['-t', '0', '--inline', '-o', '-']);
-
-    rpicam.stdout.on('data', (data) => {
-        ws.send(data);
-    });
-
-    rpicam.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });
-
-    rpicam.on('close', (code) => {
-        console.log(`rpicam-vid process exited with code ${code}`);
-    });
-
-    ws.on('close', () => {
-        rpicam.kill();
-        console.log('Client disconnected');
-    });
-});
-
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+router.get('/', (req, res) => {
+    res.send('<html><body><video src="/liveCam/stream" controls autoplay></video></body></html>');
 });
 
 module.exports = router;
